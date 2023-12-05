bfe8b6c244fdff404f1ce587f33c444257a6c677
lbt05 <lbt0506@gmail.com>
2023-12-05 22:21:43 +0800

    add pascal_triangle exercise

---
+++ pascal_triangle.go
@@ -0,0 +1,15 @@
+package exercises
+
+/*
+这里需要的是返回一个杨辉三角，并且能够满足特定的格式。
+
+比如 当 n = 4时，返回的 二维数组答应出来应该是
+
+		  1
+		 1 1
+		1 2 1
+	   1 3 3 1
+*/
+func triangle(n int) []string {
+	return []string{}
+}
---
+++ pascal_triangle_test.go
@@ -0,0 +1,12 @@
+package exercises
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// TODO: 你得自己写测试了哦
+func TestTriangle(t *testing.T) {
+	assert.True(t, false)
+}
